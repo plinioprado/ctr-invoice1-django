4451fab64a1b7130661293e2f3bdddd8b0f46295
Plinio Prado <plinioprado01@gmail.com>
2024-08-27 23:24:32 -0700

    invoice1 crud in django admin

--- ctr/invoice1/models.py
+++ ctr/invoice1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Invoice1(models.Model):
+    num = models.IntegerField(default=0)
+    value = models.FloatField(default=0)
+    issue_date = models.CharField(max_length=10)
+    parts_seller_name = models.CharField(max_length=30)
+    parts_buyer_name = models.CharField(max_length=30)
+    status = models.CharField(max_length=10)
+    #pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return f"invoice #{self.num}"
